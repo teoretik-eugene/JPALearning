7cdbbd2d0922b44f69f202485284292750c9abb6
Eugene <74543647+teoretik-eugene@users.noreply.github.com>
2023-01-30 22:18:20 +0300

    Create PersonRepository.java

---
+++ src/main/java/org/example/repositories/PersonRepository.java
@@ -0,0 +1,10 @@
+package org.example.repositories;
+
+import org.example.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+
+}
